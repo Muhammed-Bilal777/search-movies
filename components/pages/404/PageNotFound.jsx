e895cd25f9efe1859ea53fef62796fbb4b72fe12
bilal <suzzanebs123@gmail.com>
2024-01-14 03:53:38 +0530

    changed some components

--- components/pages/404/PageNotFound.jsx
+++ components/pages/404/PageNotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./style.scss"
+import React from "react";
 
-function PageNotFound() {
-  return (
-    <div>PageNotFound</div>
-  )
-}
+import "./style.scss";
 
-export default PageNotFound
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
